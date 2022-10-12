d204d91ee46a756d69e2c7e4220ead1d9a41df45
superherodemigod <111621165+DevStar0202@users.noreply.github.com>
2022-10-12 07:30:48 -0400

    main

---
+++ README.md
@@ -0,0 +1,36 @@
+# Palette Generators from image
+
+> Generate color palette from image to use in TaiwindCSS
+
+> Website: <https://colorgen.dev/>
+
+![Palette generator banner](https://res.cloudinary.com/mayashavin/image/upload/w_700,q_auto,f_auto/v1599167283/colors_generator/palette-generator)
+
+## Features
+
+1. Detect and generate predominant colors of a given image.
+2. Generate related palette for each color according to TailwindCSS style.
+3. Copy & paste the palette code to use in TailwindCSS
+
+## Technologies
+
+Nuxt.js, TailwindCSS, Vercel
+
+## Build Setup
+
+```bash
+# install dependencies
+$ yarn install
+
+# serve with hot reload at localhost:3000
+$ yarn dev
+
+# build for production and launch server
+$ yarn build
+$ yarn start
+
+# generate static project
+$ yarn generate
+```
+
+For detailed explanation on how things work, check out [Nuxt.js docs](https://nuxtjs.org).
---
+++ layouts/default.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="min-h-screen">
+    <header class="py-3 sm:px-8 flex justify-between items-center">
+      <div class="flex items-center">
+        <img
+          alt="Color generator logo" 
+          src="../src/logo.webp" />
+      </div>
+      <nav class="flex items-center">
+        <a aria-label="Github" class="mx-2" href="https://github.com/devstar0202/palette-generator/" rel="noopener noreferrer" target="_blank">
+          <svg-icon name="github" class="w-6 text-gray-700 fill-current h-6 hover:text-sea-buckthorn-600"/>
+        </a>
+      </nav>
+    </header>
+    <Nuxt />
+    <footer class="text-sm text-center text-gray-600 py-5">
+      Visit <a href="http://david0202.c1.biz" rel="noopener noreferrer" target="_blank" class="underline text-sea-buckthorn-600">David's website</a>
+    </footer>
+  </div>
+</template>
+
+<style>
+html {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+
+  @apply box-border;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+}
+</style>
